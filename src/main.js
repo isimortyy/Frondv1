c7653ce4da70a0f8c5feda8143a2643ca8e68978
Your Name <you@example.com>
2024-09-19 21:34:35 -0500

    Carpetas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
+import { Quasar } from 'quasar'
+import { createPinia } from 'pinia'
+
+
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const pinia = createPinia()
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, 
+})
+
+myApp.use(pinia)
+myApp.mount('#app')
---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+
+
+
+const routes = [
+     
+]
+
+
+
+
+
+
+
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
