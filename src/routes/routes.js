661b9e73e28f2138596993f24ea6bbb50dc72934
Your Name <you@example.com>
2024-09-26 20:08:27 -0500

    Borrado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router"
 
 
 
-
 const routes = [
-     
+    
 ]
 
 
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script staup>
+
+</script>
+
+<style>
+
+
+</style>
